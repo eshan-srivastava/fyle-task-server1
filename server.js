e072a33fc8aa16f95c5353e1ba4019e2b3dc6399
Eshan Srivastava <75296791+eshan-srivastava@users.noreply.github.com>
2024-01-21 20:20:36 +0530

    Update server.js
    modified port to be read by env file

--- server.js
+++ server.js
@@ -102,7 +102,8 @@ const server = http.createServer(async (req, res) => {
     }
 });
 
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
